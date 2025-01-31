b5b78b703f60c4280c62bd5da21e13b98d177ee7
Kevin Mills <kmills@mips.com>
2025-01-30 18:23:45 -0800

    a bit of error handlilng, for missing elf file

--- testharness.py
+++ testharness.py
@@ -1,6 +1,7 @@
 import atlasexplorer
 import tarfile
 import os
+import sys
 
 # example use of AtlasExplorer library,
 # this assumes that user has ran the script with the "configure" arg
@@ -13,7 +14,11 @@ myinst.setRootExperimentDirectory("./myexperiments")
 # run an experiment with "reports" against your elf / core selelection
 # retturns exp sub dir name
 expdir = myinst.createExperiment("mandelbrot_O0.elf", "shogun")
-print("expiriment dir: " + expdir)
+if expdir is None:
+    print("error creating experiment! Check your setup")
+    sys.exit(1)
+
+print("experiment dir: " + expdir)
 # todo:  parse result reports
 
 reportnames = ["summary", "inst_counts", "inst_trace"]

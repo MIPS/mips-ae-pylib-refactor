b598923cf6557bb7578a59e3c88adc182073af6d
Nick Preiser <nickpreiser@gmail.com>
2025-06-23 13:54:38 -0700

    more wip tests

--- atlasexplorer.py
+++ atlasexplorer.py
@@ -540,7 +540,7 @@ class Experiment:
                     os.remove(filepath)
 
 
-# cmt to test workflow 8
+# cmt to test workflow 9
 class SummaryReport:
     def __init__(self, jsonfile):
         with open(jsonfile) as f:
